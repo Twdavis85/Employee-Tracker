0d14ff1fffc232cbe451c83bcebd5977ff613581
Tyler Davis <tdnc85@yahoo.com>
2021-07-17 19:53:38 -0400

    added main menu and employee prompts

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,26 +1,90 @@
 const inquirer = require("inquirer");
 const DB = require("./db");
+require("dotenv").config();
+const { printTable } = require("console-table-printer");
 
-DB.getDepts().then(dept => {
-    console.log(dept)
+function startProg() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "menu",
+      message: "MAIN MENU?",
+      choices: [
+        "Work with Employees",
+        "Work with Positions",
+        "Work with Departments",
+        "Exit",
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.menu) {
+        case "Work with Employees":
+          employeePrompt();
+          break;
+        case "Work with Positions":
+          rolePrompt();
+          break;
+        case "Work with Departments":
+          deptPrompt();
+          break;
+        default:
+          theEnd();
+      }
+    });
+}
+
+function employeePrompt() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "emplMenu",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add New Employee",
+        "View Employees by Department",
+        "Exit",
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.emplMenu) {
+        case "View All Employees":
+          allEmployees();
+          break;
+        case "Add New Employee":
+          newEmployee();
+          break;
+        case "View Employees by Department":
+          emplByDept();
+          break;
+        default:
+          theEnd();
+      }
+    });
+}
+
+DB.getDepts().then((dept) => {
+//   console.log(dept);
 });
 
-DB.addDept("social media").then(res => {
-    console.log(res)
+DB.addDept("social media").then((res) => {
+//   console.log(res);
 });
 
-DB.getRole().then(role => {
-    console.log(role)
+DB.getRole().then((role) => {
+//   console.log(role);
 });
 
-DB.addRole("Receptionist", 50000, 4).then(res => {
-    console.log(res)
+DB.addRole("Receptionist", 50000, 4).then((res) => {
+//   console.log(res);
 });
 
-DB.getEmployee().then(employee => {
-    console.log(employee)
+DB.getEmployee().then((employee) => {
+//   console.log(employee);
 });
 
-DB.addEmployee("Tiffany", "Smith", 2, 1).then(res => {
-    console.log(res)
+DB.addEmployee("Tiffany", "Smith", 2, 1).then((res) => {
+//   console.log(res);
 });
+
+startProg();
